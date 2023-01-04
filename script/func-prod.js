d6f87897122750d8db68f099adb7d43067d33f3c
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-01-04 11:41:01 -0300

    Estilização, estrutura e funcionalidade dos produtos funcionando. (Passível de ajustes, estrutura HTML5 criada via DOM)

--- script/func-prod.js
+++ script/func-prod.js
@@ -1,6 +1,34 @@
 
 
 
+const body = document.querySelector("body");
+const main = document.createElement("main");
+const section1 = document.createElement("section");
+const section2 = document.createElement("section");
+const ul = document.createElement("ul");
+const ul2 = document.createElement("ul");
+const aside = document.createElement("aside"); // Carrinho
+const h2Primeira = document.createElement("h2");
+const h2Segunda = document.createElement("h2");
+
+body.appendChild(main);
+main.appendChild(h2Primeira);
+main.appendChild(section1);
+main.appendChild(h2Segunda);
+main.appendChild(section2);
+section1.appendChild(aside); // Carrinho
+section1.className = "ajuste"
+section2.className = "ajuste1"
+aside.className = "car" // Carrinho
+h2Primeira.className = "h2";
+h2Primeira.innerText = "Lançamentos";
+h2Segunda.className = "h2";
+h2Segunda.innerText = "Camisetas";
+section1.appendChild(ul);
+section2.appendChild(ul2);
+ul.className = "container1";
+ul2.className = "container2";
+
 
 
 
@@ -20,6 +48,7 @@ function separacaoDeItens(lista) {
         }
     }
 }
+separacaoDeItens(arrayItens);
 
 
 // Tags dentro da "li" - Função cria itens e adiciona classes
@@ -52,8 +81,8 @@ function createLiItens(objeto) {
 // Função criada para empurrar a li/itens criados para a ul
 
 function selectionListObject(listaAlt) {
-    const UlSelect = document.getElementsByClassName("container1");
-    const UlSelect2 = document.getElementsByClassName("container2");
+    const UlSelect = document.querySelector(".container1");
+    const UlSelect2 = document.querySelector(".container2");
 
     for (let i = 0; i < listaAlt.length; i++) {
         if(listaAlt === camisas) {
@@ -62,7 +91,6 @@ function selectionListObject(listaAlt) {
             UlSelect2.appendChild(createLiItens(listaAlt[i]));
         }
     }
-
 }
 
 selectionListObject(camisas);
--- script/produtos.js
+++ script/produtos.js
@@ -7,7 +7,7 @@ let arrayItens = [
     {id: "camisas", nome: "Ai Q Brisa - Amarela", price: "R$89.97", type: "Camisas", img: "./img/aiqbrisa-yellow-edit.jpg", button: "COMPRAR", p: "113 em estoque" },
     {id: "camisas", nome: "MSTREE - Vermelha", price: "R$89.97", type: "Camisas", img: "./img/mstree-red-edit.jpg", button: "COMPRAR", p: "88 em estoque" },
     {id: "camisas", nome: "MSTREE - Branca", price: "R$89.97", type: "Camisas", img: "./img/mstree-white-edit.jpg", button: "COMPRAR", p: "98 em estoque" },
-    {id: "camiseta", nome: "Classic - Laranja", price: "R$69.97", type: "Camisetas", img: "./img/classic-orange-edit.jpg", button: "COMPRAR", p: "298 em estoque" },
+    {id: "camisas", nome: "Classic - Laranja", price: "R$69.97", type: "Camisas", img: "./img/classic-orange-edit.jpg", button: "COMPRAR", p: "298 em estoque" },
     {id: "camiseta", nome: "Classic - Preta", price: "R$69.97", type: "Camisetas", img: "./img/classic-black-edit.jpg", button: "COMPRAR", p: "138 em estoque" },
     {id: "camiseta", nome: "Dorimainstreet - Roxa", price: "R$89.97", type: "Camisetas", img: "./img/dorimainstreet-purple-edit.jpg", button: "COMPRAR", p: "158 em estoque" },
     {id: "camiseta", nome: "SNT - Branca", price: "R$79.97", type: "Camisetas", img: "./img/snt-white-edite.jpg", button: "COMPRAR", p: "177 em estoque" },
